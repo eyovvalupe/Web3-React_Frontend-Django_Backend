c09432828dfe8871f884b07de333d2bcbdf6ffc4
eyovvalupe <rylantwcn705172@hotmail.com>
2024-06-24 18:38:42 +0800

    clean home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,58 +1,11 @@
 import React, { useEffect } from 'react'
 import useAuth from '../hooks/useAuth'
 import useUser from '../hooks/useUser';
-import { ethers, getDefaultProvider, formatEther, isAddress } from 'ethers';
-import { useState } from 'react';
+import Balance from './auth/Ballance';
 
 export default function Home() {
-    const [balance, setBalance] = useState(null);
-    const [provider, setProvider] = useState(null);
     const { user } = useAuth();
     const getUser = useUser()
-
-    useEffect(() => {
-        if(user.wallet_address) {
-            const initProvider = () => {
-            // Connect to the Ethereum network
-            const provider = getDefaultProvider('mainnet'); // Use 'ropsten', 'rinkeby', etc. for testnets
-            setProvider(provider);
-            };
-            initProvider();
-        }
-
-        if(user.wallet_address) {
-            getBalance()
-        }
-    }, []);
-
-    useEffect(() => {
-        if(user.wallet_address) {
-            getBalance()
-        }
-    }, [provider, user]);
-
-    const getBalance = async () => {
-        if(provider) {
-            if (!isAddress(user?.wallet_address)) {
-                alert('Please enter a valid wallet address');
-                return;
-            }
-            
-            try {
-            // Fetch the balance
-            const balance = await provider.getBalance(user?.wallet_address);
-
-            // Convert balance from Wei to Ether
-            const balanceInEther = formatEther(balance);
-
-            // Update state
-            setBalance(balanceInEther);
-            } catch (error) {
-            console.error('Error fetching balance:', error);
-                alert('Error fetching balance');
-            }
-        }
-    };
 
     useEffect(() => {
         getUser()
@@ -63,7 +16,7 @@ export default function Home() {
             <h2>
                 <div className='row'>
                     <div className="mb-12">
-                        {user?.email !== undefined ? 'List user Ethereum balance' : 'Please login first'} {balance}
+                        {user?.email !== undefined ? 'List user Ethereum balance' : 'Please login first'} <hr/> <Balance />
                     </div>
                 </div>
             </h2>
