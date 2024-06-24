0b351f78bc98977a7da3ed23ec5a5314dfc0943d
eyovvalupe <rylantwcn705172@hotmail.com>
2024-06-24 18:12:17 +0800

    add wallet address into register

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -8,6 +8,7 @@ export default function Register() {
     const first_name = useRef()
     const last_name = useRef()
     const email = useRef()
+    const wallet = useRef()
     const password = useRef()
     const password2 = useRef(undefined)
 
@@ -18,6 +19,7 @@ export default function Register() {
             first_name: first_name.current.value,
             last_name: last_name.current.value,
             email: email.current.value,
+            wallet_address: wallet.current.value,
             password: password.current.value,
             password2: password2.current.value
           };
@@ -48,6 +50,9 @@ export default function Register() {
                 </div>
                 <div className="mb-3">
                     <input type="email" placeholder='Email' autoComplete='off' className='form-control' id="email" ref={email} />
+                </div>
+                <div className="mb-3">
+                    <input type="text" placeholder='Wallet Account Address' autoComplete='off' className='form-control' id="wallet" ref={wallet} />
                 </div>
                 <div className="mb-3">
                     <input type="password" placeholder='Password' autoComplete='off' className='form-control' id="password" ref={password} />
